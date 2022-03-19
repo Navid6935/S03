88b8c1cff20a77ad047dbda5a3990d1b1bd1f3cd
Navid <navid.shatel@gmial.com>
2022-03-19 14:13:40 +0330

    handeling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { lorem } from "faker";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'S03';
+  title = "S03";
+  randomText: string = lorem.sentence();
+  onChangeInput(text: string) {
+    console.log(text);
+  }
 }
